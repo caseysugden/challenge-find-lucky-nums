d3890d952a51724dacb217ac1b4d6cfc1285d1b3
caseysugden <66505022+caseysugden@users.noreply.github.com>
2022-02-15 10:15:50 -0700

    Update findLuckyNumbers.js

--- challenge-find-lucky-nums/findLuckyNumbers.js
+++ challenge-find-lucky-nums/findLuckyNumbers.js
@@ -1,1 +1,16 @@
 // Write your code below this line.
+function nFunction(n) {
+    let array = [];
+    while (array.length < n) {
+        let tempVal = Math.ceil(Math.random() * 10);
+        if (array.includes(tempVal)) {
+            continue;
+        } else {
+            array.push(tempVal);
+        }
+    }
+    return array;
+}
+
+n = 4;
+console.log(nFunction(n));
